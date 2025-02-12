0d285b47350fa357370cb40d6fdd59745469c928
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-02-12 09:06:32 -0700

    Update Feb10.cs

--- Feb10.cs
+++ Feb10.cs
@@ -1,3 +1,4 @@
 // this is my journal prompt for february 10th
 Console.WriteLine("Yay, February 13th!");
 Console.Write("I'm turning 14!!");
+"what did i learn" I learnt how to make a C# code and I wrote down 2 lines about my birthday​
