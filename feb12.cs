60aed8d8619ce5a71e11cf399dfde4e4b5fceec5
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-02-12 09:14:25 -0700

    Update feb12.cs

--- feb12.cs
+++ feb12.cs
@@ -1,6 +1,6 @@
 //this is y code for feb 12
 //we learnt about different character types
-//did not like having to do work after cleaning up science fair
+//I didn't like how the learn thing was never ending and when i thought it was done it just kept going
 Console.WriteLine('b');
 Console.WriteLine("Hello world!");
 Console.WriteLine(123);
