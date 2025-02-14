6f5b603c0e9d2491e3c8dfb705204aa5e22b918c
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-02-14 09:58:23 -0700

    Update feb14.cs

--- feb14.cs
+++ feb14.cs
@@ -1,1 +1,7 @@
 //this my feb 14th journal prompt
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+"what did i learn" nothing it was all very confusing
