b5ee49fc70bd84dd832d69ba07d62bf273ae56ea
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-02-19 08:40:46 -0700

    Create feb19.cs

---
+++ feb19.cs
@@ -0,0 +1,10 @@
+// we learnt how to store variables
+string firstName;
+firstName = "bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
