3cff4f70e4ecd5a61bb1311a38c3040e75e1fbf8
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-03-07 10:04:41 -0700

    march 7th.cs

---
+++ march 7th.cs
@@ -0,0 +1,13 @@
+// this is my journal prompt for march 7
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+// i learnt how to use variables and how to combine them using strings
