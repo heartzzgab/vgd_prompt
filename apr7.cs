beb4f6b18463a4b813c5f6119b216caaeb1efea6
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-04-07 13:49:51 -0600

    Create apr7.cs

---
+++ apr7.cs
@@ -0,0 +1,9 @@
+// mod 5 unit 1 and 2
+// we learnt how to do addition
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
