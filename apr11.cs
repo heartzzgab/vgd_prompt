2b57579c11eecbcfbbae5efa01e32595ea8f611f
heartzzgab <Gabriella-EsperanzaH369@stemcollegiate.ca>
2025-04-11 10:00:21 -0600

    Create apr11.cs

---
+++ apr11.cs
@@ -0,0 +1,6 @@
+// module5 unit 5 and 6
+// i learnt how to use celcius and farenheit equations
+
+int fahrenheit = 94;
+decimal Celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + Celsius + " Celsius.") ;
